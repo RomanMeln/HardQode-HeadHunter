2651830c2ef22a199a8cbaf6ab15ef3cc9368b25
RomanMeln <tupozarabotok@gmail.com>
2024-03-03 10:11:02 +0300

    begin

--- HardQode_test_task_HH/education_system/models.py
+++ HardQode_test_task_HH/education_system/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField('Название', max_length=300)
+    autor = models.CharField('ФИО', max_length=100)
+    date = models.DateTimeField('Дата')
+    price = models.PositiveIntegerField('Стоимость')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Lesson(models.Model):
+    title = models.CharField('Название', max_length=200)
+    video = models.FileField('Видео')
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+
+class Group(models.Model):
+    title = models.CharField('Название', max_length=200)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
+class Student(models.Model):
+    name = models.CharField('Имя', max_length=50)
+    surname = models.CharField('Фамилия', max_length=50)
+    patronymic = models.CharField('Отчество', max_length=50)
+    email = models.EmailField()
+    date = models.DateTimeField('Дата')
+    groups = models.ForeignKey(Group,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+
+
--- HardQode_test_task_HH/education_system/views.py
+++ HardQode_test_task_HH/education_system/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Product, Lesson, Group, Student
 
-# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    lessons = Lesson.objects.all()
+    groups = Group.objects.all()
+    students = Student.objects.all()
+    return render(
+        request,
+        'education_system/index.html',
+        {'title': 'ЯЗЫКИ ПРОГРАММИРОВАНИЯ',
+        'products': products,
+        'lessons': lessons,
+        'groups': groups,
+        'students': students}
+        )
+
+def about(request):
+    return render(request, 'education_system/about.html')
